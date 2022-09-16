ca73dad2b015d558a7b83803f468c3d2787735ec
Jakkreerat <45251463+jakkreerat2540@users.noreply.github.com>
2022-09-16 09:39:59 +0700

    upgrade docker-compose to 2.11.0

--- docker-installer.sh
+++ docker-installer.sh
@@ -10,7 +10,7 @@ install_docker_compose () {
 
     rm -rf /usr/local/bin/docker-compose
     #install docker-compose
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/2.11.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
     systemctl enable docker.socket 
