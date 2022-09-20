bd1be96faa7ac5a88142943885aa10e43db3b798
Jakkreerat <45251463+jakkreerat2540@users.noreply.github.com>
2022-09-20 15:23:21 +0700

    Update docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -10,9 +10,9 @@ install_docker_compose () {
 
     rm -rf /usr/local/bin/docker-compose
     #install docker-compose
-    curl -L "https://github.com/docker/compose/releases/download/2.11.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
     systemctl enable docker.socket 
     systemctl start docker.socket
     systemctl enable containerd 
