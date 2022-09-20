3ff93d053d8436c2e2c4fa3830212da88aec441c
Jakkreerat <45251463+jakkreerat2540@users.noreply.github.com>
2022-09-20 15:34:48 +0700

    Update docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -14,6 +14,7 @@ install_docker_compose () {
     mkdir -p $DOCKER_CONFIG/cli-plugins
     curl -SL https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
     chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+    ln -s $DOCKER_CONFIG/cli-plugins/docker-compose /usr/local/bin/docker-compose
     
     # show version docker compose
     docker compose version
